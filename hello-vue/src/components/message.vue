f4cb42cf92a189a3ddc6b35f7b43ccb068a2ae14
1190100 <1190100@isep.ipp.pt>
2020-10-08 15:28:40 +0100

    commit lesson 16/17: install webpack, and run the server, hot-reload and how to import export etc.

---
+++ hello-vue/src/components/message.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box">
+    <p>
+    <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.box {
+  background: grey;
+  padding: 10px;
+  border: 1px solid;
+
+}
+
+</style>
